f4f65c424d252aaa866541dd65a0c6e0ea46810e
KumaStark <elastic48@msn.com>
2020-07-23 22:16:20 +0800

    ES6 - Day 3 - AfterLesson

---
+++ 03-1.inLesson/single.js
@@ -0,0 +1,28 @@
+// export default class Person{
+//     // 静态属性；
+//     static instance = null;
+//     constructor(name){
+//         this.name = name;
+//         if(!Person.instance){
+//             Person.instance = this;
+//         }
+//         return Person.instance;
+//     }
+// }
+
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+let instance = null;
+export default function(...arg){
+    if(!instance){
+        instance = new Person(...arg);
+    }
+    return instance;
+}
+
+
+// export default instance;
